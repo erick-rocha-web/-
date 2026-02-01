9c632ce599bbb9691f98303fab67214ec8ff4523
erick-rocha-web <erick.rocha.web@gmail.com>
2026-02-01 01:55:18 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+const form = document.querySelector(".form")
+
+const masc = document.querySelector(".masc")
+
+function AbrirForm(){
+    form.style.left = "50%"
+    form.style.transform = "translateX(-50%)"
+    masc.style.visibility = "visible"
+    form.style.transition = "1s"
+    fale.style.left = "50%"
+    fale.style.transition = "1s"
+}
+function FecharForm(){
+    form.style.left = "-350px"
+    form.style.transform = "translateX(0%)"
+    masc.style.visibility = "hidden"
+    form.style.transition = "1s"
+
+}
+const btn = document.getElementById('meubotão');
+const secao = document.getElementById('destino');
+
+btn.addEventListener('click', () => {
+  secao.scrollIntoView({ behavior: 'smooth' });
+});
